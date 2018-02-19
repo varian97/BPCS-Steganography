fd57d068bee6a903befd3864a6bf86316a168919
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2018-02-19 18:44:39 +0700

    Restructure conjugate map

--- message.py
+++ message.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import vigenere_cipher
 import math
+from pprint import pprint
 
 chessboard = np.array([[0, 1, 0, 1, 0, 1, 0, 1],
 						[1, 0, 1, 0, 1, 0, 1, 0],
@@ -24,7 +25,8 @@ class Message(object):
 	content_bitplane = []
 	header_bitplane = []
 	plane_additional_data = []
-	conjugate_map = []
+	conjugate_list = []
+	conjugation_map = []
 
 	bitplane_array = []
 	threshold = 0
@@ -79,10 +81,10 @@ class Message(object):
 		for r in range(8):
 			for c in range(8):
 				if(r != 7):
-					if(self.content_bitplane[i][r][c] != self.content_bitplane[i][r+1][c]):
+					if(self.bitplane_array[i][r][c] != self.bitplane_array[i][r+1][c]):
 						counter += 1
 				if(c != 7):
-					if(self.content_bitplane[i][r][c] != self.content_bitplane[i][r][c+1]):
+					if(self.bitplane_array[i][r][c] != self.bitplane_array[i][r][c+1]):
 						counter += 1
 		return counter / 112
 
@@ -92,22 +94,20 @@ class Message(object):
 			complexity = self.calculate_complexity(i)
 			# print(complexity)
 			if complexity < self.threshold:
-				print("blok {} terkonjugasi".format(i))
-				self.content_bitplane[i] = self.conjugate(self.content_bitplane[i])
-				self.conjugate_map.append(i)
+				# print("blok {} terkonjugasi".format(i))
+				self.bitplane_array[i] = self.conjugate(self.bitplane_array[i])
+				self.conjugate_list.append(i)
 
 	def create_message_content(self):
 		content_binary = self.to_binary(self.content)
 		self.content_bitplane = self.to_bitplane(content_binary)
-		self.conjugate_message_content()
 
 	# format header:
-	# len conjugate map ; conjugate map ; file_name ; file_extension ; content_length
+	# len file_name ; file_extension ; content_length
 	# num header: 5
 	def create_message_header(self):
 		msg_header_string = ""
 		temp = []
-		msg_header_string += str(self.conjugate_map).strip('[]') + ";"
 		msg_header_string += self.file_name + ";"
 		msg_header_string += self.file_extension + ";"
 		msg_header_string += str(self.content_length) + ";"
@@ -157,11 +157,58 @@ class Message(object):
 		self.bitplane_array += self.convert_int_to_matrix_plane(num_plane)
 		self.bitplane_array += self.content_bitplane
 
-		print(num_header, num_plane, len(self.bitplane_array))
+		# conjugate map shits
+		self.conjugate_message_content()
+		conj_map = self.create_conjugation_map()
+		conj_bitplane = self.convert_int_to_matrix_plane(len(conj_map))
+		conj_bitplane += conj_map
+
+		self.bitplane_array = conj_bitplane + self.bitplane_array
+		# print(num_header, num_plane, len(self.bitplane_array))
 		return self.bitplane_array
+
+	def create_conjugation_map(self):
+		conj_map = ['0' for i in range(len(self.bitplane_array))]
+		for pos in self.conjugate_list:
+			conj_map[pos] = '1'
+
+		while(len(conj_map) % 8 != 0):
+			conj_map.append('0')
+
+		conj_binary = [''.join(conj_map[x:x+8]) for x in range(0, len(conj_map), 8)]
+		while(len(conj_binary) % 8 != 0):
+			conj_binary.append('01010101')
+		# print(len(conj_map))
+		# pprint (conj_binary)
+		self.conjugation_map = self.to_bitplane(conj_binary)
+
+		for i in range(len(self.conjugation_map)):
+			self.conjugation_map[i] = self.conjugate(self.conjugation_map[i])
+		return self.conjugation_map
+
+	def deconjugate_bitplane(self, bitplane):
+		for i in range(len(self.conjugation_map)):
+			self.conjugation_map[i] = self.conjugate(self.conjugation_map[i])
+
+		conj_map = []
+		for plane in self.conjugation_map:
+			conj_map.append(''.join([''.join(row) for row in plane.astype(str)]))
+		conj_map = ''.join(conj_map)
+
+		for i in range(len(conj_map)):
+			if (conj_map[i] == '1'):
+				bitplane[i] = self.conjugate(bitplane[i])
+
+		return bitplane
 
 	def from_bitplane_array(self, bitplane_array):
 		# print(len(bitplane_array))
+		conj_map_length = self.matrix_to_int(bitplane_array[0])
+		self.conjugation_map = bitplane_array[1:conj_map_length + 1]
+
+		bitplane_array = bitplane_array[conj_map_length+1:]
+
+		bitplane_array = self.deconjugate_bitplane(bitplane_array)
 
 		header_length = self.matrix_to_int(bitplane_array[0])
 		self.header_bitplane = bitplane_array[1:header_length + 1]
@@ -197,15 +244,15 @@ class Message(object):
 
 		header_chunk = self.header.split(';')
 		print(header_chunk)
-		self.conjugate_map = []
-		if header_chunk[0] != '':
-			for conjugate_pos in header_chunk[0].split(','):
-				self.conjugate_map.append(int(conjugate_pos))
+		self.conjugate_list = []
+		# if header_chunk[0] != '':
+		# 	for conjugate_pos in header_chunk[0].split(','):
+		# 		self.conjugate_list.append(int(conjugate_pos))
 
-		self.file_name = header_chunk[1]
-		self.file_extension = header_chunk[2]
-		self.content_length = int(header_chunk[3])
-		# print(self.conjugate_map)
+		self.file_name = header_chunk[0]
+		self.file_extension = header_chunk[1]
+		self.content_length = int(header_chunk[2])
+		# print(self.conjugate_list)
 		# print(self.content_length)
 		# print(self.file_extension)
 		# print(self.file_name)
@@ -213,8 +260,6 @@ class Message(object):
 		return self.header
 
 	def get_content_from_bitplanes(self):
-		for pos in self.conjugate_map:
-			self.content_bitplane[pos] = self.conjugate(self.content_bitplane[pos])
 		content = self.get_byte_from_bitplane_array(self.content_bitplane)
 		self.content = content[:self.content_length]
 
@@ -233,10 +278,28 @@ class Message(object):
 		with open(file_name, 'wb') as fout:
 			fout.write(self.content)
 
-if __name__ == "__main__":
-	msg = Message(pathname='testcase/message/textpanjang.txt', encrypted = True, key ='supersecret')
-	bitplane_msg = msg.create_message()
+def calculate_complexity(img):
+    counter = 0
+    for r in range(8):
+        for c in range(8):
+            if(r != 7):
+                if(img[r][c] != img[r+1][c]):
+                    counter += 1
+            if(c != 7):
+                if(img[r][c] != img[r][c+1]):
+                    counter += 1
+    return counter / 112
 
-	msg12 = Message(encrypted = True, key ='supersecret')
+if __name__ == "__main__":
+	msg = Message(pathname='testcase/original_img/watch.png')
+	bitplane_msg = msg.create_message()
+	print(len(bitplane_msg))
+	for b in bitplane_msg:
+		# print (calculate_complexity(b))
+		if (calculate_complexity(b) <= 0.3):
+			print(b)
+
+	# print(bitplane_msg)
+	msg12 = Message()
 	msg12.from_bitplane_array(bitplane_msg)
 	msg12.write_msg()
